9e4d97db24d91d0ce04fda0f8002c9d81f6517f3
patimue <patimue@hotmail.de>
2022-08-30 23:37:30 +0200

    Added Pull info

--- src/app/components/user-item/user-item.component.ts
+++ src/app/components/user-item/user-item.component.ts
@@ -26,7 +26,7 @@ export class UserItemComponent implements OnInit {
   ngOnInit(): void {
     this.userId = this.route.snapshot.paramMap.get('id') ?? "";
     this.isLoadingFinished = true; 
-    // this.getUserInfo();
+    this.getUserInfo();
   }
 
   getUserInfo() {
@@ -65,7 +65,7 @@ export class UserItemComponent implements OnInit {
           }
           for (let playerElem of user.pl) {
             this.players.push({
-              name: playerElem.fn + " " + playerElem.n,
+              name: playerElem.fn ?? "" + " " + playerElem.n,
               number: playerElem.nr,
               points: playerElem.t,
               image: playerElem.i
