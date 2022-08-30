6c99a8fe66e9a1de1b64b214083ee3794a292391
Paul <patimue@hotmail.de>
2022-08-30 12:46:28 +0200

    Added new Request

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,9 +8,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  checkStorage() {
+    const token = localStorage.getItem('ctoken');
+    const leagueid = localStorage.getItem('leagueid');
+
+    if (token === undefined || leagueid === undefined) {
+      this.router.navigate(['/login']);
+    }
+    fetch(`https://europe-west1-kickbase-dashboard.cloudfunctions.net/getMatchDay?token=${token}&leagueId=${leagueid}`, {
+      method: "GET",
+      redirect: "follow"
+    })
+    .then(async (res) => {
+      let text = await res.text();
+      console.log(text);
+    })
+    .catch((error) => {
+      console.log(error);
+    })
   }
 
 }
